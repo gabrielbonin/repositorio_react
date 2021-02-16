692ed6b108d591d1129f3d88d760475e740498f8
gabriel bonin <gabrielmatallo1998@gmail.com>
2021-02-16 14:46:37 -0300

    consumindo api do github e buscando repositorios

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, {useState, useCallback} from 'react';
 import * as S from './styles';
 import {FaGithub, FaPlus} from 'react-icons/fa';
+import api from '../../services/api';
 export default function Main(){
+
+  const [newRepo, setNewRepo] = useState('');
+  const [repositorios, setRepositorios] = useState([]);
+
+
+   const handleSubmit = useCallback((e)=>{
+    e.preventDefault();
+    
+    async function submit(){
+      const response =  api.get(`repos/${newRepo}`);
+      const data = {
+      name: response.data.full_name,
+    }
+    setRepositorios([...repositorios, data]);
+    setNewRepo('');
+    }
+    submit();
+
+  }, [newRepo, repositorios]);
+
+  function handleinputChange(e){
+    setNewRepo(e.target.value);
+  }
+
   return(
     <div>
       <S.Container>
@@ -9,8 +34,11 @@ export default function Main(){
           <FaGithub size={38}/>
           Meus repositorios
         </h1>
-          <S.Form onSubmit={()=>{}}>
-              <input type="text" placeholder="adicionar repositorio"/>
+          <S.Form onSubmit={handleSubmit}>
+              <input type="text" placeholder="adicionar repositorio"
+              value={newRepo}
+              onChange={handleinputChange}
+              />
               <S.SubmitButton>
               <FaPlus color="#FFF" size={14}/>
               </S.SubmitButton>
