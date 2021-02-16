9228e22227c807069592a1639f53735da6a70585
gabriel bonin <gabrielmatallo1998@gmail.com>
2021-02-16 13:43:51 -0300

    Estilização da Pagina Main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import * as S from './styles';
+import {FaGithub, FaPlus} from 'react-icons/fa';
 export default function Main(){
   return(
     <div>
-      <S.Title>
-        Main
-      </S.Title>
+      <S.Container>
+        <h1>
+          <FaGithub size={38}/>
+          Meus repositorios
+        </h1>
+          <S.Form onSubmit={()=>{}}>
+              <input type="text" placeholder="adicionar repositorio"/>
+              <S.SubmitButton>
+              <FaPlus color="#FFF" size={14}/>
+              </S.SubmitButton>
+        </S.Form>
+      </S.Container>
     </div>
   );
 }
